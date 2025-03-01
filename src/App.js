0509604c400974cc349c9c59ba252bff3d06bb88
Duc Toan <toanbeo942000@gmail.com>
2025-03-01 11:30:02 -0800

    form checkbox, radio cơ bản

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+const courses = [
+  {
+    id: 1,
+    name: "html",
+  },
+  { id: 2, name: "css" },
+  { id: 3, name: "react" },
+];
 function App() {
+  const [check, setCheck] = useState();
+  const handlerSubmit = () => {
+    console.log(check);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {courses.map((value) => {
+        return (
+          <div key={value.id}>
+            <input
+              type="radio"
+              checked={check === value.id}
+              onChange={() => setCheck(value.id)}
+            />
+            {value.name}
+          </div>
+        );
+      })}
+      <button onClick={handlerSubmit}>register</button>
+    </div>
+  );
+}
+
+function Checkbox() {
+  const [check, setCheck] = useState([]);
+
+  const handlerCheck = (id) => {
+    setCheck((pre) => {
+      const isCheck = check.includes(id);
+      if (isCheck) {
+        return check.filter((item) => item !== id);
+      } else {
+        return [...pre, id];
+      }
+    });
+  };
+  const handlerSubmit = () => {
+    console.log(check);
+  };
+  return (
+    <div className="App">
+      {courses.map((value) => {
+        return (
+          <div key={value.id}>
+            <input
+              type="checkbox"
+              checked={check.includes(value.id)}
+              onChange={() => handlerCheck(value.id)}
+            />
+            {value.name}
+          </div>
+        );
+      })}
+      <button onClick={handlerSubmit}>register</button>
     </div>
   );
 }
