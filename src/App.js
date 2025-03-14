4b166e4af57d235eaf4cad535aed506a59cbec7e
Duc Toan <toanbeo942000@gmail.com>
2025-03-14 15:00:08 -0700

    useContext

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import MyContextProvider from "./asset/useContext/useContext";
+import Page1 from "./asset/useContext/page_1";
 import Image from "./asset/useEffect/image";
 import Todo from "./asset/useReducer/todo";
 function App() {
@@ -7,6 +9,10 @@ function App() {
       <Image />
       {/* thực hành chức nằng useReducer với ứng dụng todo list */}
       <Todo />
+      {/* thực hành chức nằng useContext  */}
+      <MyContextProvider>
+        <Page1 />
+      </MyContextProvider>
     </>
   );
 }
